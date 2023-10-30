5ea12fe42bf85a856f72dce9cb06bea80d6ea45c
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-30 16:44:50 +0000

    square

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -3,4 +3,14 @@ class CalculatorController < ApplicationController
   def home_page
     render template: "calc_templates/home"
   end
+
+  def square_new
+    render template: "calc_templates/square_new"
+  end
+
+  def square_results
+    @number = params.fetch(:number)
+    @square = @number.to_f ** 2
+    render template: "calc_templates/square_results"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,59 @@
 Rails.application.routes.draw do
   get "/", controller: "calculator", action: "home_page"
-
+  get "/square/new", controller: "calculator", action: "square_new"
+  get "/square/results", controller: "calculator", action: "square_results"
 end
+
+=begin
+get("/square/results") do
+  @number = params.fetch(:number)
+  @square = @number.to_f ** 2
+
+  erb(:square_results, { :layout => :layout })
+end
+
+get("/square_root/new") do
+  
+  erb(:square_root, { :layout => :layout })
+end
+
+get("/square_root/results") do
+  @number = params.fetch(:number)
+  @square_root = @number.to_f ** 0.5
+
+  erb(:square_root_results, { :layout => :layout })
+end
+
+get("/payment/new") do
+  
+  erb(:payment, { :layout => :layout })
+end
+
+get("/payment/results") do
+  @apr = params.fetch(:apr).to_f
+  @time = params.fetch(:time)
+  @length = @time.to_i*12
+  @principal = params.fetch(:principal).to_f
+  @r = @apr/1200
+
+  @numerator = @r*@principal
+  @denominator = 1-(1+@r)**(-@length)
+  @payment = (@numerator/@denominator)
+
+  erb(:payment_results, { :layout => :layout })
+end
+
+get("/random/new") do
+  
+  erb(:random, { :layout => :layout })
+end
+
+get("/random/results") do
+  @min = params.fetch(:floor).to_f
+  @max = params.fetch(:ceiling).to_f
+  @variable = rand(@min..@max)
+
+  erb(:random_results, { :layout => :layout })
+end
+
+=end
